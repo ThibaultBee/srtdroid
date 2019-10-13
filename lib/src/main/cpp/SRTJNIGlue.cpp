a4c4a69e0f1177f724a4dbd6c0c18d00e4213e23
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2019-10-13 21:42:32 +0200

    feat(SRTJNIGlue.cpp): add a simple logger

--- lib/src/main/cpp/SRTJNIGlue.cpp
+++ lib/src/main/cpp/SRTJNIGlue.cpp
@@ -1,9 +1,17 @@
 #include <jni.h>
 #include <string>
-
+#include <android/log.h>
 #include "srt/srt.h"
 
 extern "C" {
+
+#define TAG "JNIGlue"
+
+// Log tools
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 
 // Library Initialization
 JNIEXPORT jint JNICALL
