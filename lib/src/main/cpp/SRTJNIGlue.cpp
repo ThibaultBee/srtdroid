6dbfecde08408be9f29331fc6955c30edf0fb253
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2019-10-13 21:48:25 +0200

    feat(SRTJNIGlue.cpp): Add a logger for SRT lib

--- lib/src/main/cpp/SRTJNIGlue.cpp
+++ lib/src/main/cpp/SRTJNIGlue.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include <android/log.h>
 #include "srt/srt.h"
+#include "srt/logging_api.h"
 
 extern "C" {
 
 #define TAG "JNIGlue"
+#define SRTLIB_TAG "SRTLIB"
 
 // Log tools
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
@@ -13,9 +15,38 @@ extern "C" {
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 
+// Logger
+void srt_logger(void *opaque, int level, const char *file, int line, const char *area,
+                const char *message) {
+    int android_log_level = ANDROID_LOG_UNKNOWN;
+
+    switch (level) {
+        case LOG_CRIT:
+            android_log_level = ANDROID_LOG_FATAL;
+            break;
+        case LOG_ERR:
+            android_log_level = ANDROID_LOG_ERROR;
+            break;
+        case LOG_WARNING:
+            android_log_level = ANDROID_LOG_WARN;
+            break;
+        case LOG_NOTICE:
+            android_log_level = ANDROID_LOG_INFO;
+            break;
+        case LOG_DEBUG:
+            android_log_level = ANDROID_LOG_DEBUG;
+            break;
+        default:
+            LOGE("Unknown log level %d", level);
+    }
+
+    __android_log_print(android_log_level, SRTLIB_TAG, "%s@%d:%s %s", file, line, area, message);
+}
+
 // Library Initialization
 JNIEXPORT jint JNICALL
 Java_com_github_thibaultbee_srtwrapper_Srt_nativeStartUp(JNIEnv *env, jobject obj) {
+    srt_setloghandler(nullptr, srt_logger);
     return srt_startup();
 }
 
