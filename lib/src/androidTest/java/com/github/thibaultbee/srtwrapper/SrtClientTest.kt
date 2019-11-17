e3d9663853619dd1802c0238ca387063704a06ba
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2019-11-17 13:06:29 +0100

    test(SrtClientTest): add an equivalent of test-c-client to test an SRT connection between a server and a client

---
+++ lib/src/androidTest/java/com/github/thibaultbee/srtwrapper/SrtClientTest.kt
@@ -0,0 +1,55 @@
+package com.github.thibaultbee.srtwrapper
+
+import android.Manifest.permission.ACCESS_NETWORK_STATE
+import android.util.Log
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.thibaultbee.srtwrapper.enums.SockOpt
+import com.github.thibaultbee.srtwrapper.models.Error
+import com.github.thibaultbee.srtwrapper.models.Socket
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import android.Manifest.permission.INTERNET
+import androidx.test.rule.GrantPermissionRule
+import org.junit.Rule
+import java.lang.Thread.sleep
+
+@RunWith(AndroidJUnit4::class)
+class SrtClientTest {
+    @Rule @JvmField
+    /*
+     * Grant android.permission.INTERNET so that SRT can use socket
+     */
+    var socketPermissionRule = GrantPermissionRule.grant(INTERNET)
+
+    private val ip = "10.0.2.2" // emulator host loopback interface
+    private val port = 1234
+    private val msg = "Hello from SRT Android"
+
+    /*
+     * To be use with test-c-server from SRT library examples. Build your own SRT library with ENABLE_EXAMPLES flags.
+     * Default IP and port are meant to be execute with an emulator. In this case, run on your host: ./test-c-server 127.0.0.1 1234
+     * Otherwise, you will have to set ip, port and test-c-server parameters according to their network configuration
+     */
+    @Test
+    fun srtClientTest() {
+        val srt = Srt()
+
+        assertEquals(0, srt.startUp())
+
+        val socket = Socket()
+        assertTrue(socket.isValid())
+
+        assertEquals(0, socket.setSockOpt(SockOpt.SENDER, 1))
+        assertEquals(0, socket.connect(ip, port))
+
+        for (i in 1..20) {
+            assertEquals(msg.length, socket.sendMsg2(msg))
+        }
+
+        sleep(1000) // If session is close too early, msg will not be receive by server
+        assertEquals(0, socket.close())
+        assertEquals(0, srt.cleanUp())
+    }
+}
