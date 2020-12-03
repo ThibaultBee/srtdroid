b09ec0d33d04f6f426e51fabb67b945ae6d63019
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2020-12-03 23:21:34 +0100

    feat(tests): Add transmission file test

---
+++ lib/src/androidTest/java/com/github/thibaultbee/srtdroid/models/transmission/FileTest.kt
@@ -0,0 +1,91 @@
+package com.github.thibaultbee.srtdroid.models.transmission
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.thibaultbee.srtdroid.Srt
+import com.github.thibaultbee.srtdroid.enums.SockOpt
+import com.github.thibaultbee.srtdroid.enums.Transtype
+import com.github.thibaultbee.srtdroid.models.Socket
+import com.github.thibaultbee.srtdroid.utils.Utils
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+import java.net.InetAddress
+import java.util.*
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+
+
+/*
+ * Theses tests are written to check if SRT API can be called from the Kotlin part.
+ */
+
+@RunWith(AndroidJUnit4::class)
+class FileTest {
+    private val srt = Srt()
+    private lateinit var socket: Socket
+
+    @Before
+    fun setUp() {
+        assert(srt.startUp() >= 0)
+        socket = Socket()
+        socket.setSockFlag(SockOpt.TRANSTYPE, Transtype.FILE)
+        assertTrue(socket.isValid)
+    }
+
+    @After
+    fun tearDown() {
+        socket.close()
+        assertEquals(srt.cleanUp(), 0)
+    }
+
+    @Test
+    fun recvFileTest() {
+        val server = MockSendServer()
+        val message = UUID.randomUUID().toString()
+        val sendFile = Utils.createTestFile(message = message)
+
+        server.enqueue(sendFile)
+        socket.connect(InetAddress.getLoopbackAddress(), server.port)
+
+        val recvFile = File(
+            InstrumentationRegistry.getInstrumentation().context.externalCacheDir,
+            UUID.randomUUID().toString()
+        )
+        assertEquals(sendFile.length(), socket.recvFile(recvFile, size = sendFile.length()))
+    }
+
+    internal class MockSendServer {
+        private val executor = Executors.newCachedThreadPool()
+        private val serverSocket = Socket()
+        val port: Int
+        private var socket: Socket? = null
+
+        init {
+            serverSocket.reuseAddress = true
+            serverSocket.setSockFlag(SockOpt.TRANSTYPE, Transtype.FILE)
+            serverSocket.bind(InetAddress.getLoopbackAddress(), 0)
+            port = serverSocket.localPort
+        }
+
+        fun enqueue(file: File): Future<Long?> {
+            return executor.submit(Callable {
+                serverSocket.listen(1)
+                val pair = serverSocket.accept()
+                assertNotNull(pair.second)
+                socket = pair.first
+                socket?.sendFile(file)
+            })
+        }
+
+        fun shutdown() {
+            socket?.close()
+            serverSocket.close()
+            executor.shutdown()
+        }
+    }
+}
