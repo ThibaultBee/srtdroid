3b792b007be3bb209aa588c3c8c8384a4586bc90
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2021-03-11 16:20:43 +0100

    docs(Srt): add dokka API documentation

--- lib/src/main/java/com/github/thibaultbee/srtdroid/Srt.kt
+++ lib/src/main/java/com/github/thibaultbee/srtdroid/Srt.kt
@@ -15,16 +15,59 @@
  */
 package com.github.thibaultbee.srtdroid
 
+/**
+ * This class provides main SRT control. Before calling any other SRT API, you shall call [startUp].
+ */
 class Srt {
     init {
         System.loadLibrary("jnisrt")
     }
 
+    /**
+     * Called at the start of an application that uses the SRT library.
+     *
+     * **See Also:** [srt_startup](https://github.com/Haivision/srt/blob/master/docs/API-functions.md#srt_startup)
+     *
+     * @return a value >= 0 on success, otherwise -1
+     * @see [cleanUp]
+     */
     external fun startUp(): Int
+
+    /**
+     * Cleans up all global SRT resources and shall be called just before exiting the application (for example, in your main activity onRelease()).
+     *
+     * **See Also:** [srt_cleanup](https://github.com/Haivision/srt/blob/master/docs/API-functions.md#srt_cleanup)
+     *
+     * @return a reserved for future use value
+     * @see [startUp]
+     */
     external fun cleanUp(): Int
+
     private external fun nativeGetVersion(): Int
+
+    /**
+     * Get SRT version
+     *
+     * **See Also:** [srt_getversion](https://github.com/Haivision/srt/blob/master/docs/API-functions.md#srt_getversion)
+     *
+     * @return hexadecimal version (0xXXYYZZ - x.y.z)
+     */
     val version: Int
         get() = nativeGetVersion()
 
+    /**
+     * Set log level
+     *
+     * Log level value are the constant from <sys/syslog.h>:
+     * - LOG_CRIT:	    2	critical conditions
+     * - LOG_ERR:		3	error conditions
+     * - LOG_WARNING:	4	warning conditions
+     * - LOG_NOTICE:    5	normal but significant condition
+     * - LOG_DEBUG:	    7	debug-level messages
+     *
+     * **See Also:** [srt_setloglevel](https://github.com/Haivision/srt/blob/master/docs/API-functions.md#srt_setloglevel)
+     *
+     * @param level log level
+     */
     external fun setLogLevel(level: Int)
 }
